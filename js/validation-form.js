f98f2c5a2b75358fa25ece81f4fc617499b2d8d2
BlueDokk <katzschwarz@gmail.com>
2021-02-25 12:00:17 -0500

    Update commit

--- js/validation-form.js
+++ js/validation-form.js
@@ -11,51 +11,52 @@ const fields = {};
 
 window.onload = () => {
 
-  let inputs = document.querySelectorAll('.form__input');
-  let errorMessage = document.getElementById('form__message');
-
-  var validateField = (expresion, input, field) => {
-    if (expresion.test(input.value)) {
-      document.getElementById(`group__${field}`).classList.remove('form__group-incorrect');
-      document.getElementById(`group__${field}`).classList.add('form__group-correct');
-      document.querySelector(`#group__${field} i`).classList.remove('fa-times-circle');
-      document.querySelector(`#group__${field} i`).classList.add('fa-check-circle');
-      document.querySelector(`#form__error-${field} p`).classList.remove('form__input-error-active');
-      fields[field] = true;
-      if (Object.values(fields).includes(false)) {
-        errorMessage.style.display = 'block';
-        console.log('false');
+  setTimeout(()=>{
+    let inputs = document.querySelectorAll('.form__input');
+    let errorMessage = document.getElementById('form__message');
+    var validateField = (expresion, input, field) => {
+      if (expresion.test(input.value)) {
+        document.getElementById(`group__${field}`).classList.remove('form__group-incorrect');
+        document.getElementById(`group__${field}`).classList.add('form__group-correct');
+        document.querySelector(`#group__${field} i`).classList.remove('fa-times-circle');
+        document.querySelector(`#group__${field} i`).classList.add('fa-check-circle');
+        document.querySelector(`#form__error-${field} p`).classList.remove('form__input-error-active');
+        fields[field] = true;
+        if (Object.values(fields).includes(false)) {
+          errorMessage.style.display = 'block';
+          console.log('false');
+        } else {
+          errorMessage.style.display = 'none';
+        }
       } else {
-        errorMessage.style.display = 'none';
+        document.getElementById(`group__${field}`).classList.add('form__group-incorrect');
+        document.getElementById(`group__${field}`).classList.remove('form__group-correct');
+        document.querySelector(`#group__${field} i`).classList.add('fa-times-circle');
+        document.querySelector(`#group__${field} i`).classList.remove('fa-check-circle');
+        document.querySelector(`#form__error-${field} p`).classList.add('form__input-error-active');
+        errorMessage.style.display = 'block';
+        fields[field] = false;
       }
-    } else {
-      document.getElementById(`group__${field}`).classList.add('form__group-incorrect');
-      document.getElementById(`group__${field}`).classList.remove('form__group-correct');
-      document.querySelector(`#group__${field} i`).classList.add('fa-times-circle');
-      document.querySelector(`#group__${field} i`).classList.remove('fa-check-circle');
-      document.querySelector(`#form__error-${field} p`).classList.add('form__input-error-active');
-      errorMessage.style.display = 'block';
-      fields[field] = false;
-    }
-  };
-
-  var validateFields = function (e) {
-    switch (e.target.id) {
-      case 'name':
-        validateField(expresions.name, e.target, 'name');
-        break;
-      case 'lastname':
-        validateField(expresions.lastname, e.target, 'lastname');
-        break;
-      case 'email':
-        validateField(expresions.email, e.target, 'email');
-        break;
-    }
-  };
-  inputs.forEach(input => {
-    input.addEventListener('keyup', validateFields);
-    input.addEventListener('blur', validateFields);
-  });
+    };
+  
+    var validateFields = function (e) {
+      switch (e.target.id) {
+        case 'name':
+          validateField(expresions.name, e.target, 'name');
+          break;
+        case 'lastname':
+          validateField(expresions.lastname, e.target, 'lastname');
+          break;
+        case 'email':
+          validateField(expresions.email, e.target, 'email');
+          break;
+      }
+    };
+    inputs.forEach(input => {
+      input.addEventListener('keyup', validateFields);
+      input.addEventListener('blur', validateFields);
+    });
+  },100);
 };
 
 function validateForm() {
